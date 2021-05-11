b0a5de6d311634eb483d53d9f4f08cc4bcb4ecc5
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-05-12 00:09:32 +0900

    feat(baseball) : 서로 다른 임의의 수 3개를 생성하는 기능 구현

--- java-baseball-precourse1/src/main/java/baseball/Application.java
+++ java-baseball-precourse1/src/main/java/baseball/Application.java
@@ -1,6 +1,9 @@
 package baseball;
 
 import baseball.domain.Calculator;
+import baseball.domain.NumberGenerator;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -8,5 +11,9 @@ public class Application {
         //1. 기능을 가지고 있는 클래스를 인스턴스화 한다.
         //2. 필요한 기능을 각 인스터스가 수행하게 한다. (의인화)
         //3. 각 결과를 종합한다.(합친다)
+
+        final NumberGenerator generator= new NumberGenerator();
+        final List<Integer> numbers = generator.createRandomNumbers();
+        System.out.println(numbers);
     }
 }
