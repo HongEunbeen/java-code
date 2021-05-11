b8da5b9c03be0423f9c8bb2a162e106955baf386
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-05-12 00:25:22 +0900

    feat(baseball) : 컴퓨터의 수와 플레이어의 수를 비교할 수 있는 기능 구현

--- java-baseball-precourse1/src/main/java/baseball/Application.java
+++ java-baseball-precourse1/src/main/java/baseball/Application.java
@@ -3,6 +3,7 @@ package baseball;
 import baseball.domain.Calculator;
 import baseball.domain.Judgment;
 import baseball.domain.NumberGenerator;
+import baseball.domain.Referee;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
@@ -10,20 +11,8 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        //객체 지향 프로그래밍
-        //1. 기능을 가지고 있는 클래스를 인스턴스화 한다.
-        //2. 필요한 기능을 각 인스터스가 수행하게 한다. (의인화)
-        //3. 각 결과를 종합한다.(합친다)
-
-        final NumberGenerator generator= new NumberGenerator();
-        final List<Integer> numbers = generator.createRandomNumbers();
-        System.out.println(numbers);
-
-        Judgment judgment = new Judgment();
-        final int count = judgment.correctCount(Arrays.asList(1,2,3), Arrays.asList(1,2,3));
-        System.out.println(count);
-
-        final boolean place = judgment.hasPlace(Arrays.asList(7,8,9), 0, 7);
-        System.out.println(place);
+        final Referee referee = new Referee();
+        final String result = referee.compare(Arrays.asList(3,1,2),Arrays.asList(3,1,2));
+        System.out.println(result);
     }
 }
--- java-baseball-precourse1/src/main/java/baseball/domain/Referee.java
+++ java-baseball-precourse1/src/main/java/baseball/domain/Referee.java
@@ -4,6 +4,21 @@ import java.util.List;
 
 public class Referee {
     public String compare(List<Integer> computer, List<Integer> player){
-        return "3 스트라이그";
+        //볼(몇개의 숫자가 같은지)의 개수를 먼저 구한 뒤
+        //스트라이크의 개수를 구해 뺀다 = 볼의 개수
+        Judgment judgment = new Judgment();
+        int correctCount = judgment.correctCount(computer, player);
+
+        int strike = 0;
+        for(int placeIndex =0; placeIndex< computer.size(); placeIndex++){
+            judgment.hasPlace(computer, placeIndex, player.get(placeIndex));
+            strike++;
+        }
+        int ball = correctCount - strike;
+        //if(ball != 0)
+        if(strike == 0){
+            return "아웃";
+        }
+        return ball + " 볼 " + strike + " 스크라이크";
     }
 }
